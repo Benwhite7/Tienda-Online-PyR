2d9077f524e2c875bb7cf4b8665279577ad88315
Benwhite7 <melendezcastilloemer@gmail.com>
2023-03-17 10:21:23 -0500

    avanze 2}

--- src/components/Thirdannouncement.jsx
+++ src/components/Thirdannouncement.jsx
@@ -4,10 +4,12 @@ import "../styles/Thirdannouncement.scss"
 const Thirdannouncement = () => {
     return (
         <div className="thirdannouncement">
-            <img src="https://i.pinimg.com/564x/66/ab/98/66ab985ea289c1e4f81579da0e355d3b.jpg" alt="" />
+            <div className="thirdimage">
+                <img src="https://i.pinimg.com/564x/66/ab/98/66ab985ea289c1e4f81579da0e355d3b.jpg" alt="" />
+            </div>
             <div className="Linkthird">
-                <h1>Zapatillas para mujer</h1>
-                <p>Zapatillas para mujer <br /> de las mejores marcas</p>
+                <h1>Zapatillas para dama</h1>
+                <p>Calzado para dama de las mejores marcas</p>
                 <a href="/women">Descubre más </a>
             </div>
         </div>
--- src/pages/Start.jsx
+++ src/pages/Start.jsx
@@ -5,20 +5,24 @@ import "../styles/Start.scss";
 
 const Start = () => {
     return (
-        <div className="Allcontainer">
-            <div className="firstannouncement">
-                <img src="https://preview.thenewsmarket.com/Previews/ADID/StillAssets/1920x1080/635883.jpg" alt="" />
+        <>
+            <div className="Allcontainer">
+                <div className="firstannouncement">
+                    <img src="https://preview.thenewsmarket.com/Previews/ADID/StillAssets/1920x1080/635883.jpg" alt="" />
+                </div>
                 <div className="Link">
                     <h1>PREDATOR ACCURACY+ FG</h1>
                     <p>Descubre lo mejor en chimpunes y zapatillas para hacer deporte.</p>
                     <a href="/sports">Comprar</a>
                 </div>
             </div>
+
             <div className="announcement">
                 <Secondannouncement />
                 <Thirdannouncement />
             </div>
-        </div>
+        </>
+
     )
 }
 
